57bf29d64dd4a0609ba2214d003417b0cc78d267
ROHIT ROY <143118462+rohitroyoo@users.noreply.github.com>
2026-07-20 00:39:12 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        String rev = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+
+        return s.equals(rev);
+    }
+}
