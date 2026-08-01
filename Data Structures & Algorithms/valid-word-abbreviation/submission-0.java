780e2b9e9459bdcf501d2b0738541c8b0ae98958
ROHIT ROY <143118462+rohitroyoo@users.noreply.github.com>
2026-08-01 12:20:58 +0530

    Add: valid-word-abbreviation - submission-0

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+
+        int i = 0;
+        int j = 0;
+
+        while(i < word.length() && j < abbr.length()){
+            char ch = abbr.charAt(j);
+
+            if(Character.isLetter(ch)){
+                if(word.charAt(i) != ch){
+                    return false;
+                }
+                i++;
+                j++;
+            } else{
+
+                if(ch == '0'){
+                    return false;
+                }
+
+                int num = 0;
+
+                 while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    num = num * 10 + (abbr.charAt(j) - '0');
+                    j++;
+                }
+
+                i += num;
+            }
+        }
+        return i == word.length() && j == abbr.length();
+    }
+}
