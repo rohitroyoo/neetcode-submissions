d02e5bd89aad03da43cf935c22c10aad5a879968
ROHIT ROY <143118462+rohitroyoo@users.noreply.github.com>
2026-08-08 00:41:05 +0530

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) {
+            return nums[0];
+        }
+        if(n == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int case1 = robDP(nums, 0, n - 2);
+
+        int case2 = robDP(nums, 1, n - 1);
+
+        return Math.max(case1, case2);
+    }
+
+    private int robDP(int[] nums, int start, int end) {
+
+        int n = end - start + 1;
+
+        int[] dp = new int[n];
+
+        dp[0] = nums[start];
+
+        if (n == 1) {
+            return dp[0];
+        }
+
+        dp[1] = Math.max(nums[start], nums[start + 1]);
+
+        for (int i = 2; i < n; i++) {
+
+            int currentHouse = nums[start + i];
+
+            dp[i] = Math.max(
+                dp[i - 1],
+                currentHouse + dp[i - 2]
+            );
+        }
+
+        return dp[n - 1];
+    }
+}
