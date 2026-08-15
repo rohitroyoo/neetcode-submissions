a547a6c47311e6a8f189ac61eccbdc5f0c57b8dc
ROHIT ROY <143118462+rohitroyoo@users.noreply.github.com>
2026-08-15 23:52:02 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int[][] dp = new int[n][2];
+
+        // Day 0
+        dp[0][0] = 0;           // Don't hold
+        dp[0][1] = -prices[0];  // Buy stock
+
+        for (int i = 1; i < n; i++) {
+
+            // Don't hold stock
+            dp[i][0] = Math.max(
+                dp[i - 1][0],
+                dp[i - 1][1] + prices[i]
+            );
+
+            // Hold stock
+            dp[i][1] = Math.max(
+                dp[i - 1][1],
+                dp[i - 1][0] - prices[i]
+            );
+        }
+
+        return dp[n - 1][0];
+    }
+}
